f7e54154633a3488234abf4e88fd9e58a633ab58
xanonDev <52205282+xanonDev@users.noreply.github.com>
2023-09-16 10:58:52 +0200

    adding support for css and local images, etc and converting links from <a> into page requests to our server

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import requests
 import base64
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
 
 app = Flask(__name__)
 
@@ -8,8 +10,30 @@ app = Flask(__name__)
 @app.route('/bypass')
 def bypass():
   link = request.args.get("link")
-  content = requests.get(base64.b64decode(link))
-  content = base64.b64encode(content.content)
+  link = base64.b64decode(link).decode('utf-8')
+  content = requests.get(link)
+  soup = BeautifulSoup(content.text, 'html.parser')
+  for tag in soup.find_all(href=True):
+    href = tag['href']
+    parsed_href = urlparse(href)
+    if not parsed_href.scheme or not parsed_href.netloc:
+      public_link = urljoin(link, href)
+      tag['href'] = public_link
+    else:
+      pass
+  for tag in soup.find_all(src=True):
+    src = tag['src']
+    parsed_src = urlparse(src)
+    if not parsed_src.scheme or not parsed_src.netloc:
+      public_link = urljoin(link, src)
+      tag['src'] = public_link
+  for a_tag in soup.find_all('a', href=True):
+    href = a_tag['href']
+    encodedlink = base64.b64encode(href.encode('utf-8'))
+    encodedlink = encodedlink.decode('utf-8')
+    encodedlink = f'/bypass?link={encodedlink}'
+    a_tag['href'] = encodedlink
+  content = base64.b64encode(soup.prettify().encode('utf-8'))
   webchanger = '''
       <input id="link"></input>
     <button id="submit">change website</button>
